73a87fe530fe73c3e2d4dc3d5ad96f5beb12a3fa
sweeywu <14862177+sweeywu@user.noreply.gitee.com>
2025-03-19 12:59:20 +0800

    groupcachetest

--- groupcache_test.go
+++ groupcache_test.go
@@ -1,7 +1,11 @@
 package groupcache
 
 import (
+	"errors"
 	"fmt"
+	pb "groupcache/groupcachepb"
+	"groupcache/singleflight"
+	"hash/crc32"
 	"sync"
 	"testing"
 	"time"
@@ -115,4 +119,141 @@ func TestEnviction(t *testing.T) {
 	if fills != 1 {
 		t.Errorf("expected 1 fill,got %d", fills)
 	}
+}
+
+type fackPeer struct {
+	hits int
+	fail bool
+}
+
+// 让他可以使用peers里面的这个
+//
+//	type PeerGetter interface {
+//		Get(*pb.GetRequest, *pb.GetResponse) error
+//	}
+//
+// 这个是调用远程节点是用的
+func (f *fackPeer) Get(request *pb.GetRequest, response *pb.GetResponse) error {
+	f.hits++
+	if f.fail {
+		return errors.New("fail")
+	}
+	response.Value = []byte("got:" + request.GetKey())
+	return nil
+}
+
+type fackPeers []PeerGetter
+
+func (f fackPeers) PickPeer(key string) (PeerGetter, bool) {
+	if len(f) == 0 {
+		return nil, false
+	}
+	n := crc32.Checksum([]byte(key), crc32.IEEETable) % uint32(len(f))
+	return f[n], f[n] != nil
+}
+func TestPeers(t *testing.T) {
+	once.Do(testSetup)
+	peer0 := &fackPeer{}
+	peer1 := &fackPeer{}
+	peer2 := &fackPeer{}
+	peersList := fackPeers([]PeerGetter{peer0, peer1, peer2, nil})
+	localHits := 0
+	//这个是本地调用
+	get := func(key string, dst Sink) error {
+		localHits++
+		return dst.SetString("got:" + key)
+	}
+	testGroup := newGroup("test-peers-group", 0, GetterFunc(get), peersList)
+	run := func(name string, n int) {
+		localHits = 0
+		for _, p := range peersList {
+			p.(*fackPeer).hits = 0
+		}
+		for i := 0; i < n; i++ {
+			key := fmt.Sprintf("key-%d")
+			want := "got:" + key
+			var got string
+			if err := testGroup.Get(key, StringSink(&got)); err != nil {
+				t.Fatal(err)
+			}
+			if got != want {
+				t.Fatal("bad value")
+			}
+		}
+	}
+	resetCacheSize := func(cachesize int64) {
+		g := testGroup
+		g.mainCache = cache{}
+		g.hotCache = cache{}
+		g.cacheBytes = cachesize
+	}
+	resetCacheSize(1 << 20)
+	run("bbb", 200)
+	run("ggg", 200)
+	resetCacheSize(0)
+	peersList[0] = nil
+	run("one_peer_done", 200)
+	peersList[0] = peer0
+	peer0.fail = true
+	run("peer0_fail", 200)
+}
+func TesttruncBytesSink(t *testing.T) {
+	var buf [100]byte
+	s := buf[:]
+	if err := StringGroup.Get("wxnzb", TruncBytesSink(&s)); err != nil {
+		t.Fatal(err)
+	}
+	if want := "ECHO:wxnzb"; string(s) != want {
+		t.Errorf("want %q,got %q", want, s)
+	}
+	s = buf[:6]
+	if err := StringGroup.Get("wxnzb", TruncBytesSink(&s)); err != nil {
+		t.Fatal(err)
+	}
+	if want := "ECHO:w"; string(s) != want {
+		t.Errorf("want %q,got %q", want, s)
+	}
+}
+
+// 确保 dst、inBytes 和 view.b 之间 不共享内存
+func TestallocBytesSink(t *testing.T) {
+	var dst []byte
+	sink := AllocBytesSink(&dst)
+	inBytes := []byte("hello wx!")
+	sink.SetBytes(inBytes)
+	if want := "hello wx!"; string(dst) != want {
+		t.Errorf("want %q,got %q", want, dst)
+	}
+	v, err := sink.view()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if &inBytes[0] == &v.b[0] {
+		t.Error("inBytes and dst share memory")
+	}
+	if &inBytes[0] == &dst[0] {
+		t.Error("inBytes and dst share memory")
+	}
+	if &v.b[0] == &dst[0] {
+		t.Error("inBytes and dst share memory")
+	}
+}
+
+// 证 singleflight 无法去重的情况下 groupcache 是否还能正确缓存
+type orderedFlight struct {
+	mu     sync.Mutex
+	stage1 chan bool
+	stage2 chan bool
+	org    *singleflight.Group
+}
+
+func (o *orderedFlight) Do(key string, fn func() (interface{}, error)) (interface{}, error) {
+	<-o.stage1
+	<-o.stage2
+	mu.Lock()
+	defer mu.Unlock()
+	return o.org.Do(key, fn)
+}
+func testNodup(t *testing.T) {
+
 }
